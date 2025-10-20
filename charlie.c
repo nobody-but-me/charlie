9c375b5a852554c587f29b98910abb1c0199a416
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-10-20 19:10:46 -0300

    Left column.

--- charlie.c
+++ charlie.c
@@ -6,6 +6,7 @@
 #include <termios.h>
 #include <ctype.h>
 
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,22 +20,43 @@
 // |-                   Definition                 -|
 // \------------------------|-----------------------/
 
+struct editorConfig {
+    struct termios m_OriginalTermios;
+    unsigned int screenRows;
+    unsigned int screenCols;
+};
+
+int getWindowSize(unsigned int *rows, unsigned int *cols);
+
 void error(const char *errorMessage);
 
 void disableRawMode(void);
 void enableRawMode(void);
 
-void refresh_screen(void);
-void draw_rows(void);
+void refreshScreen(void);
+void drawRows(void);
 
-void key_press(void);
-char read_key(void);
+void keyPress(void);
+char readKey(void);
+
+void init(void);
 
 // /------------------------|-----------------------\
 // |-                 Implementation               -|
 // \------------------------|-----------------------/
 
-struct termios gOriginalTermios;
+
+struct editorConfig g_Configuration;
+
+
+int getWindowSize(unsigned int *rows, unsigned int *cols) {
+    struct winsize window_size;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size) == -1 || window_size.ws_col == 0)
+	return -1;
+    *cols = window_size.ws_col;
+    *rows = window_size.ws_row;
+    return 0;
+}
 
 void error(const char *errorMessage) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -44,17 +66,17 @@ void error(const char *errorMessage) {
 }
 
 void disableRawMode(void) {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &gOriginalTermios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_Configuration.m_OriginalTermios) == -1)
         error("tcsetattr");
     return;
 }
 
 void enableRawMode(void) {
-    if (tcgetattr(STDIN_FILENO, &gOriginalTermios) == -1)
+    if (tcgetattr(STDIN_FILENO, &g_Configuration.m_OriginalTermios) == -1)
         error("tcgetattr");
     atexit(disableRawMode);
     
-    struct termios raw = gOriginalTermios;
+    struct termios raw = g_Configuration.m_OriginalTermios;
     raw.c_iflag &= ~(BRKINT | ICRNL | ISTRIP | IXON);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_oflag &= ~(OPOST);
@@ -68,24 +90,24 @@ void enableRawMode(void) {
     return;
 }
 
-void refresh_screen(void) {
+void refreshScreen(void) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     
-    draw_rows();
+    drawRows();
     
     write(STDOUT_FILENO, "\x1b[H", 3);
     return;
 }
-void draw_rows(void) {
-    for (int y = 0; y < 24; y++) {
+void drawRows(void) {
+    for (unsigned int y = 0; y < g_Configuration.screenRows; y++) {
 	write(STDOUT_FILENO, COLUMN_SYMBOL"\r\n", 3);
     }
     return;
 }
 
-void key_press(void) {
-    char c = read_key();
+void keyPress(void) {
+    char c = readKey();
     switch (c) {
         case 27:
 	    write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -96,7 +118,7 @@ void key_press(void) {
     return;
 }
 
-char read_key(void) {
+char readKey(void) {
     int nread; char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 	if (nread == -1 && errno != EAGAIN)
@@ -105,11 +127,19 @@ char read_key(void) {
     return c;
 }
 
+void init(void) {
+    if (getWindowSize(&g_Configuration.screenRows, &g_Configuration.screenCols) == -1)
+        error("getWindowSize");
+    return;
+}
+
 int main(void) {
     enableRawMode();
+    init();
+    
     while (1) {
-	refresh_screen();
-	key_press();
+	refreshScreen();
+	keyPress();
     }
     return 0;
 }
